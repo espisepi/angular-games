2a5ae85a5ff54f395083ad9a37a025b7fed22693
espisepi <sepinaco@gmail.com>
2024-09-03 14:54:53 +0200

    Refactor RendererEngine

--- src/lib/threejs/engine/core/RendererEngine.ts
+++ src/lib/threejs/engine/core/RendererEngine.ts
@@ -4,21 +4,36 @@ import { getElementHeight, getElementWidth } from './FunctionLibrary';
 
 export class RendererEngine {
 
+  public clock: THREE.Clock;
+	public renderDelta: number;
+	public logicDelta: number;
+	public requestDelta?: number;
+	public sinceLastFrame: number;
+	public justRendered: boolean;
+
+  // ============================
+
   public renderer: THREE.WebGLRenderer;
 
   public width: number;
   public height: number;
 
   public camera: THREE.PerspectiveCamera;
+  public scene: THREE.Scene;
   public parent: HTMLElement;
 
-  constructor(parent: HTMLElement, camera: THREE.PerspectiveCamera) {
+  // ===========================
+
+  constructor(parent: HTMLElement, camera: THREE.PerspectiveCamera, scene: THREE.Scene) {
 
     this.parent = parent;
     this.camera = camera;
+    this.scene = scene;
 
     this.width = getElementWidth(parent);
     this.height = getElementHeight(parent);
+
+    /// ============================================
 
     // Renderer
 		this.renderer = new THREE.WebGLRenderer();
@@ -41,9 +56,51 @@ export class RendererEngine {
 		}
 		window.addEventListener('resize', onWindowResize, false);
 
+    // =========================================
+
+    // RenderLoop
+		this.clock = new THREE.Clock();
+		this.renderDelta = 0;
+		this.logicDelta = 0;
+		this.sinceLastFrame = 0;
+		this.justRendered = false;
+
+
   }
 
-  render(): void {
+  render(): number[] {
+    this.requestDelta = this.clock.getDelta();
+
+
+		// Getting timeStep
+		let unscaledTimeStep = (this.requestDelta + this.renderDelta + this.logicDelta) ;
+		// let timeStep = unscaledTimeStep * this.params.Time_Scale;
+    let timeStep = unscaledTimeStep * 1;
+		timeStep = Math.min(timeStep, 1 / 30);    // min 30 fps
+
+    	// Measuring logic time
+		this.logicDelta = this.clock.getDelta();
+
+		// Frame limiting
+		let interval = 1 / 60;
+		this.sinceLastFrame += this.requestDelta + this.renderDelta + this.logicDelta;
+		this.sinceLastFrame %= interval;
+
+		// Stats end
+		// this.stats.end();
+		// this.stats.begin();
+
+		// Actual rendering with a FXAA ON/OFF switch
+		// if (this.params.FXAA) this.composer.render();
+		// else this.renderer.render(this.graphicsWorld, this.camera);
+    this.renderer.render(this.scene, this.camera);
+
+		// Measuring render time
+		this.renderDelta = this.clock.getDelta();
+
+    return [ timeStep, unscaledTimeStep ];
+
+
 
   }
 }
--- src/lib/threejs/engine/world/WorldEngine.ts
+++ src/lib/threejs/engine/world/WorldEngine.ts
@@ -15,13 +15,6 @@ export class WorldEngine {
 
   public graphicsWorld: THREE.Scene;
 
-  public clock: THREE.Clock;
-	public renderDelta: number;
-	public logicDelta: number;
-	public requestDelta?: number;
-	public sinceLastFrame: number;
-	public justRendered: boolean;
-
   public inputManager: InputManager;
   public cameraOperator: CameraOperator;
 
@@ -44,8 +37,6 @@ export class WorldEngine {
 
   constructor(options: IWorldEngineOptions) {
     const { parent } = options;
-    const scope = this;
-
 
     const width = getElementWidth(parent);
     const height = getElementHeight(parent);
@@ -56,15 +47,8 @@ export class WorldEngine {
     this.camera.position.set(0,0,3);
 
     // Renderer
-    this.rendererEngine = new RendererEngine(parent, this.camera);
+    this.rendererEngine = new RendererEngine(parent, this.camera, this.graphicsWorld);
 
-
-    // RenderLoop
-		this.clock = new THREE.Clock();
-		this.renderDelta = 0;
-		this.logicDelta = 0;
-		this.sinceLastFrame = 0;
-		this.justRendered = false;
 
     // Initialization
 		this.inputManager = new InputManager(this, this.rendererEngine.renderer.domElement);
@@ -85,41 +69,17 @@ export class WorldEngine {
 	 */
 	public render(): void
 	{
-    		this.requestDelta = this.clock.getDelta();
 
 		requestAnimationFrame(() =>
 		{
 			this.render();
 		});
 
-		// Getting timeStep
-		let unscaledTimeStep = (this.requestDelta + this.renderDelta + this.logicDelta) ;
-		let timeStep = unscaledTimeStep * this.params.Time_Scale;
-		timeStep = Math.min(timeStep, 1 / 30);    // min 30 fps
+    // Update render RenderEngine
+    const [ timeStep, unscaledTimeStep ] = this.rendererEngine.render();
 
 		// Logic
 		this.update(timeStep, unscaledTimeStep);
-
-		// Measuring logic time
-		this.logicDelta = this.clock.getDelta();
-
-		// Frame limiting
-		let interval = 1 / 60;
-		this.sinceLastFrame += this.requestDelta + this.renderDelta + this.logicDelta;
-		this.sinceLastFrame %= interval;
-
-		// Stats end
-		// this.stats.end();
-		// this.stats.begin();
-
-		// Actual rendering with a FXAA ON/OFF switch
-		// if (this.params.FXAA) this.composer.render();
-		// else this.renderer.render(this.graphicsWorld, this.camera);
-    this.rendererEngine.renderer.render(this.graphicsWorld, this.camera);
-
-		// Measuring render time
-		this.renderDelta = this.clock.getDelta();
-
 
   }
 
@@ -133,39 +93,16 @@ export class WorldEngine {
   public updateControls(controls: any): void
 	{
     console.log("Update Controls, new Controls: " + controls);
-		// let html = '';
-		// html += '<h2 class="controls-title">Controls:</h2>';
-
-		// controls.forEach((row: any) =>
-		// {
-		// 	html += '<div class="ctrl-row">';
-		// 	row.keys.forEach((key: any) => {
-		// 		if (key === '+' || key === 'and' || key === 'or' || key === '&') html += '&nbsp;' + key + '&nbsp;';
-		// 		else html += '<span class="ctrl-key">' + key + '</span>';
-		// 	});
-
-		// 	html += '<span class="ctrl-desc">' + row.desc + '</span></div>';
-		// });
-
-		// document.getElementById('controls').innerHTML = html;
 	}
 
   // Update
 	// Handles all logic updates.
 	public update(timeStep: number, unscaledTimeStep: number): void
 	{
-		// this.updatePhysics(timeStep);
-
 		// Update registred objects
 		this.updatables.forEach((entity) => {
 			entity.update(timeStep, unscaledTimeStep);
 		});
-
-		// Lerp time scale
-		// this.params.Time_Scale = THREE.MathUtils.lerp(this.params.Time_Scale, this.timeScaleTarget, 0.2);
-
-		// Physics debug
-		// if (this.params.Debug_Physics) this.cannonDebugRenderer.update();
 	}
 
 

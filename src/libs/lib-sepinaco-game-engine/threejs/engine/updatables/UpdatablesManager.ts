3498906db25149b7b22bd543b017ab119480fde3
espisepi <sepinaco@gmail.com>
2024-09-05 13:24:20 +0200

    added unregisterUpdatables in UpdatablesManager

--- src/libs/lib-sepinaco-game-engine/threejs/engine/updatables/UpdatablesManager.ts
+++ src/libs/lib-sepinaco-game-engine/threejs/engine/updatables/UpdatablesManager.ts
@@ -19,6 +19,11 @@ export class UpdatablesManager {
 		this._updatables.sort((a, b) => (a.updateOrder > b.updateOrder) ? 1 : -1);
 	}
 
+  public unregisterUpdatable(registree: IUpdatable): void
+	{
+		this._updatables = this._updatables.filter(item => item !== registree);
+	}
+
 
   public update(timeStep: number, unscaledTimeStep: number): void {
     // Update registred objects

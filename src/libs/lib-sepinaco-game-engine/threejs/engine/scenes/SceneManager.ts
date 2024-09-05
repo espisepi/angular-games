c8cbd6402ff05d45274047a3189d0da56b35e2c9
espisepi <sepinaco@gmail.com>
2024-09-05 03:05:25 +0200

    added TODO:

---
+++ src/libs/lib-sepinaco-game-engine/threejs/engine/scenes/SceneManager.ts
@@ -0,0 +1,12 @@
+
+
+// TODO: Hacer los scenarios o cambios de escenas y tener en cuenta el dispose de los objetos: https://threejs.org/manual/#en/cleanup
+// TODO: Seguir la misma arquitectura que con controls: manager, factory, features
+// https://threejs.org/manual/#en/cleanup
+// Resumen:
+// boxGeometry.dispose();
+// boxTexture.dispose();
+// boxMaterial.dispose();
+export class SceneManager {
+
+}
